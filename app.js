657204037797466af384c7c9a61011794378756e
Stima360 <info@stima360.it>
2025-12-27 14:10:38 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+const verifyToken = process.env.VERIFY_TOKEN;
+
+app.get('/', (req, res) => {
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode === 'subscribe' && token === verifyToken) {
+    console.log('WEBHOOK VERIFIED');
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.post('/', (req, res) => {
+  console.log('\nWebhook received:\n');
+  console.log(JSON.stringify(req.body, null, 2));
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
